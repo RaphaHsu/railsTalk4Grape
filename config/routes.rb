fc3b46ac819f2936d9c5bd3f284a309d00b3382e
raphahsu <fc.nothung@gmail.com>
2015-05-02 10:28:47 +0800

    rake post, scope api route with json format

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :posts
+  
+  scope :api do
+    resources :posts, :defaults => { :format => 'json' }
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
